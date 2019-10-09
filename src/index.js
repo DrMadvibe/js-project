c14b02726affeb80f645271e49ac3ae7a8f3cfae
David Evans <davidevans@hastingsdirect.com>
2019-10-09 15:06:57 +0100

    add button component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import Button from './Button';
+
 ReactDom.render(
-	<h1>React application</h1>,
+	<Button />,
 	document.getElementId('app')
 );
