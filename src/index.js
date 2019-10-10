ba2e456b6981f02a8b7c0e1a3e780f7d341efb7e
David Evans <davidevans@hastingsdirect.com>
2019-10-10 12:31:48 +0100

    Add styling

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import Button from './Button';
+/*
+ReactDom.render(
+	<Button theme="primary">Primary Button</Button>,
+	document.getElementById('app')
+);
 
+ReactDom.render(
+	<Button theme="secondary">Secondary Button</Button>,
+	document.getElementById('app')
+);
+
+ReactDom.render(
+	<Button isFullWidth>Full Width Button</Button>,
+	document.getElementById('app')
+);
+*/
+ReactDom.render(
+	<Button theme="primary" isFullWidth>Primary Full Width Button</Button>,
+	document.getElementById('app')
+);
+/*
 ReactDom.render(
 	<Button url="https://www.hastingsdirect.com">Hastings Direct</Button>,
 	document.getElementById('app')
 );
+*/
